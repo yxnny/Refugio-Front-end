c65bf0823158f221e1eaf8be2c371a97c6908eb1
yxnny <sisoyenn7@gmail.com>
2023-08-13 17:07:33 -0500

    Proyecto Refugio Front End

---
+++ js/script.js
@@ -0,0 +1,66 @@
+
+function cambiarImg(){
+    document.getElementById('img1').style.display = 'block';
+    document.getElementById('img2').style.display = 'none';
+
+    document.getElementById('img1').style.animation='example 1s 1';
+    
+    document.getElementById('prueba').style.background='#4A6DAD';
+    document.getElementById('prueba2').style.background='#ffffff';
+}
+function cambiarImg2(){
+   
+    document.getElementById('img1').style.display = 'none';
+    document.getElementById('img2').style.display = 'block';
+
+    document.getElementById('img2').style.animation='example 1s 1';
+    
+    document.getElementById('prueba').style.background='#ffffff';
+    document.getElementById('prueba2').style.background='#4A6DAD';
+
+    
+    
+}
+
+
+function cambiarperro(){
+    document.getElementById('card1').style.display = 'block';
+    document.getElementById('card2').style.display = 'block';
+    document.getElementById('card3').style.display = 'block';
+    document.getElementById('card4').style.display = 'block';
+
+    document.getElementById('card5').style.display = 'none';
+    document.getElementById('card6').style.display = 'none';
+    document.getElementById('card7').style.display = 'none';
+    document.getElementById('card8').style.display = 'none';
+
+    document.getElementById('perro').style.background='#6AA5E3';
+    document.getElementById('perro').style.color='#fff';
+    document.getElementById('gato').style.background='#ffffff';
+    document.getElementById('gato').style.color='#6AA5E3';
+    document.getElementById('circulo').style.background='#F1F9FD';
+    document.getElementById('circulo2').style.background='#F1F9FD';
+
+}
+function cambiargato(){
+    document.getElementById('card1').style.display = 'none';
+    document.getElementById('card2').style.display = 'none';
+    document.getElementById('card3').style.display = 'none';
+    document.getElementById('card4').style.display = 'none';
+
+    document.getElementById('card5').style.display = 'block';
+    document.getElementById('card6').style.display = 'block';
+    document.getElementById('card7').style.display = 'block';
+    document.getElementById('card8').style.display = 'block';
+
+    document.getElementById('perro').style.background='#ffffff';
+    document.getElementById('perro').style.color='#6AA5E3';
+    document.getElementById('gato').style.background='#6AA5E3';
+    document.getElementById('gato').style.color='#ffffff';
+    document.getElementById('circulo').style.background='#F1F9FD';
+
+    document.getElementById('circulo2').style.background='#ffffff';
+    
+    
+}
+
